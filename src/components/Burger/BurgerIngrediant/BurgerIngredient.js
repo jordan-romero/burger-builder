aba716c380d03df4fdd7b34c95f2a1919aa1031a
jordles113 <jordan.t.romero@gmail.com>
2021-02-11 15:57:35 -0600

    creates type propType in BurderIng

--- src/components/Burger/BurgerIngrediant/BurgerIngredient.js
+++ src/components/Burger/BurgerIngrediant/BurgerIngredient.js
@@ -1,12 +1,12 @@
-import React from 'react'
-
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import classes from './BurgerIngredient.css'
 
-const BurgerIngredient = (props) => {
+class BurgerIngredient extends Component {
+    render () {
+        let ingredient = null; 
 
-    let ingredient = null; 
-
-    switch(props.type) {
+    switch(this.props.type) {
         case('bread-bottom'): 
             ingredient = <div className={classes.BreadBottom}></div>;
             break; 
@@ -34,6 +34,12 @@ const BurgerIngredient = (props) => {
             ingredient = null;
     }
     return ingredient
+    }
+    
+}
+
+BurgerIngredient.PropTypes = {
+    type: PropTypes.string.isRequired
 }
 
 export default BurgerIngredient
