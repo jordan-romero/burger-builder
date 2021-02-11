808474d22d1249a207dcf9b533d4e0ed4031acd5
jordles113 <jordan.t.romero@gmail.com>
2021-02-11 15:51:54 -0600

    adds switch and case statements for burgerbuilder:

--- src/components/Burger/BurgerIngrediant/BurgerIngredient.js
+++ src/components/Burger/BurgerIngrediant/BurgerIngredient.js
@@ -1,12 +1,34 @@
 import React from 'react'
 
+import classes from './BurgerIngredient.css'
+
 const BurgerIngredient = (props) => {
 
     let ingredient = null; 
 
     switch(props.type) {
         case('bread-bottom'): 
-            ingredient
+            ingredient = <div className={classes.BreadBottom}></div>;
+            break; 
+        case('bread-top'):
+            ingredient = (
+                <div className={classes.BreadTop}>
+                    <div className={classes.Seeds1}></div>
+                    <div className={classes.Seeds1}></div>
+                </div>
+            );
+            break; 
+        case('meat'):
+            ingredient = <div className={classes.Meat}></div>
+            break; 
+        case('cheese'):
+            ingredient = <div className={classes.Cheese}></div>;
+            break; 
+        case ('salad'):
+            ingredient = <div className={classes.Salad}></div>
+            break;
+        case('bacon'):
+            ingredient = <div className={classes.Bacon}></div>
     }
     return (
         <div>
