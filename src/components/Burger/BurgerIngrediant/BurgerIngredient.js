93d3492af8046fd4fc423f5a41b97298dfd8cc53
jordles113 <jordan.t.romero@gmail.com>
2021-02-11 15:45:33 -0600

    fixes typos

---
+++ src/components/Burger/BurgerIngrediant/BurgerIngredient.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const BurgerIngredient = (props) => {
+
+    let ingredient = null; 
+
+    switch(props.type) {
+        case('bread-bottom'): 
+            ingredient
+    }
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default BurgerIngredient
