cd9ad4f4485568bfbbc3f3c93b5a77be163a9d14
jordles113 <jordan.t.romero@gmail.com>
2021-02-19 18:00:31 -0600

    starts building out burger component

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import BurgerIngredient from "./BurgerIngrediant/BurgerIngredient";
+import classes from './Burger.css'
+
+const Burger = (props) => {
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type='bread-top'/>
+            <BurgerIngredient type='cheese'/>
+            <BurgerIngredient type='meat'/>
+            <BurgerIngredient type='bread-bottom'/>
+        </div>
+    ); 
+}
+
+export default Burger; 
