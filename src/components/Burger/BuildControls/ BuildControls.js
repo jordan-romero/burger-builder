3a5826adb4182d75bc490709d9aab44b795cdb26
jordles113 <jordan.t.romero@gmail.com>
2021-02-19 18:40:01 -0600

    wires up BuildControls with BuildControls.css

--- src/components/Burger/BuildControls/ BuildControls.js
+++ src/components/Burger/BuildControls/ BuildControls.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import classes from './BuildControls.css'
 
 const  BuildControls = (props) => {
     return (
-        <div>
+        <div className={classes.BuildControls}>
             
         </div>
     )
