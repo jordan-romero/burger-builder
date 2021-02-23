85ef336f1c7833c9d70159770438f8d52374813e
jordles113 <jordan.t.romero@gmail.com>
2021-02-23 16:38:23 -0600

    wires up purchaseable method for updating purchaseable in state

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -21,6 +21,22 @@ export default class BurgerBuilder extends Component {
             meat: 0
         },
         totalPrice: 4, 
+        purchaseable: false, 
+    }
+
+    updatePurchaseState = () => {
+        const ingredients = {
+            ...this.state.ingredients
+        }
+        const sum = Object.keys(ingredients)
+        .map(igKey => {
+            return ingredients[igKey]
+        })
+        .reduce((sum, el) => {
+            return sum + el
+        }, 0)
+
+        this.setState({purchaseable: sum > 0})
     }
 
     addIngredientHandler = (type) => {
@@ -69,6 +85,7 @@ export default class BurgerBuilder extends Component {
                 ingredientRemoved={this.removeIngredientHandler}
                 disabled={disabledInfo}
                 price={this.state.totalPrice}
+                purchaseable={this.state.purchaseable}
                 />
             </Aux>
         )
