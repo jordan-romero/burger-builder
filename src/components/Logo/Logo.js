3421a492992eafe8ade247fb71f38a10f7e602bf
jordles113 <jordan.t.romero@gmail.com>
2021-02-26 16:44:10 -0600

    builds out logo and styles it

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import classes from './Logo.css';
+import Logo from '../../assets/images/burger-logo.png';
 
 const Logo = () => {
-  return <div></div>;
+  return (
+    <div className={classes.Logo}>
+      <img src={Logo} alt='Burger Logo' />
+    </div>
+  );
 };
 
 export default Logo;
