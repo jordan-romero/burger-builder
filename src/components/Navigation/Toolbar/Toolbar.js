c81868e2006a08422bdfa4614f522840e494a5e4
jordles113 <jordan.t.romero@gmail.com>
2021-02-26 16:31:28 -0600

    builds out toolbar component

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Toolbar = (props) => {
+  return (
+    <header>
+      <div>MENU</div>
+      <div>LOGO</div>
+      <nav>...</nav>
+    </header>
+  );
+};
+
+export default Toolbar;
