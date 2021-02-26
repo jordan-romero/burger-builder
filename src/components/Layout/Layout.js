399848836fa1191d9dbb8a6c04bac93c3370baad
jordles113 <jordan.t.romero@gmail.com>
2021-02-26 16:36:53 -0600

    imports toolbar into layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,19 +1,16 @@
-import React from 'react'
+import React from 'react';
 
-import Aux from '../../hoc/Aux'
-import classes from './Layout.css'
+import Aux from '../../hoc/Aux';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import classes from './Layout.css';
 
 const Layout = (props) => {
-    return (
-        <Aux>
-            <div>
-                Toolbar, SideDrawer, Backdrop
-            </div>
-            <main className={classes.Content}>
-                {props.children}
-            </main>
-        </Aux>
-    )
-}
+  return (
+    <Aux>
+      <Toolbar />
+      <main className={classes.Content}>{props.children}</main>
+    </Aux>
+  );
+};
 
-export default Layout
+export default Layout;
