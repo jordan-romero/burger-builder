e268d448d2967565df87f89a47adffc2cbedac8e
jordles113 <jordan.t.romero@gmail.com>
2021-02-26 16:35:39 -0600

    adds css to Toolbar and imports css classes

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import classes from './Toolbar.css';
 
 const Toolbar = (props) => {
   return (
-    <header>
+    <header className={classes.Toolbar}>
       <div>MENU</div>
       <div>LOGO</div>
       <nav>...</nav>
